165618cf459c60249725f130ffa417b1c7304265
bidhubbr <67633833+bidhubbr@users.noreply.github.com>
2020-06-30 18:00:56 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		Promotion p = new Promotion();
+		static void Main(string[] args)
+		{
+			char ch;
+			do
+			{
+				Console.WriteLine("Enter skuids in Cart (Only Alphabets)");
+				char[] SKUIds = Console.ReadLine().ToUpper().ToCharArray();
+
+				//char[] SKUIds = new char[] { 'A', 'B', 'C'};
+				//char[] SKUIds = new char[] { 'A', 'B','C','A','A','B','A','B','B','A','B' };
+				//char[] SKUIds = new char[] { 'A', 'B', 'C', 'A', 'B', 'A', 'B', 'B', 'D', 'B' };
+
+				double amount = Promotion.ApplyPromotion(SKUIds);
+
+				Console.WriteLine("Total order Value : " + amount);
+				
+				Console.WriteLine("Want to Continue(Y/N)...");
+				
+				ch = Console.ReadKey().KeyChar;
+				Console.WriteLine("\n");
+
+
+			} while (ch == 'Y' || ch == 'y');
+		}
+		
+	}
+}
---
+++ ConsoleApp2/Promotion.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Promotion
+	{
+		public static double ApplyPromotion(Char[] skuIds)
+		{
+			double total = 0;
+			int countA = 0;
+			int countB = 0;
+			int countC = 0;
+			int countD = 0;
+			Array.Sort<char>(skuIds);			
+
+			foreach (char skuId in skuIds)
+			{
+				switch (skuId)
+				{
+					case 'A':
+						countA++; break;
+					case 'B':
+						countB++; break;
+					case 'C':
+						countC++; break;
+					case 'D':
+						countD++; break;
+					default:
+						break;
+				}
+			}
+
+			// Promotion 1
+
+			if (countA > 2)
+			{
+				total = (int)(countA / 3) * 130 + (countA % 3) * 50;
+			}
+			else
+			{
+				total = countA * 50;
+			}
+
+			// Promotion 2
+
+			if (countB > 1)
+			{
+				total = total + (int)(countB / 2) * 45 + (countB % 2) * 30;
+			}
+			else
+			{
+				total = total + countB * 30;
+			}
+
+			// Promotion 3
+
+			if (countC > 0)
+			{
+				if (countD > 0)
+				{
+					if (countC == countD)
+					{
+						total = total + countC * 30;
+					}
+					else if (countC > countD)
+					{
+						total = total + countD * 30 + (countC - countD) * 20;
+					}
+					else
+					{
+						total = total + countC * 30 + (countD - countC) * 15;
+					}
+				}
+				else
+				{
+					total = total + countC * 20;
+				}
+			}
+			else if (countD > 0)
+			{
+				total = total + countD * 15;
+			}
+
+			return total;
+		}
+	}
+}
